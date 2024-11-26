ecfae24d90110647922d2f469cd59391e46705fc
mriansyah93 <mriansyah93@gmail.com>
2024-11-26 11:23:28 +0800

    init django-uho

---
+++ django_uho/django_uho/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Root URL menuju view home
+    path('admin/', admin.site.urls),
+    # path('tebak_angka/', include('tebak_angka.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
