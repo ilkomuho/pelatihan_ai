df2b753d58ae5bee675f648891c24e4dba380081
mriansyah93 <mriansyah93@gmail.com>
2024-11-26 13:56:40 +0800

    add urls tebak_angka

--- django_uho/django_uho/urls.py
+++ django_uho/django_uho/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Root URL menuju view home
     path('admin/', admin.site.urls),
-    # path('tebak_angka/', include('tebak_angka.urls')),
+    path('tebak_angka/', include('tebak_angka.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
